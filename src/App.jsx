da77d7046a5c9f5fd555c89f9c881f5f059db04c
Hannah <hannahsenft@gmx.net>
2022-01-11 09:28:24 +0100

    add todo list functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../src/components/Button";
 
+/* const todos = [
+	{
+		name: "Buy milk",
+		isChecked: true,
+	},
+	{
+		name: "clean kitchen",
+		isChecked: false,
+	},
+	{
+		name: "water plants",
+		isChecked: false,
+	},
+]; */
+
 const App = () => {
-  return (
-    <div>
-      <h1>Hello test</h1>
-      <Button>Click me!</Button>
-    </div>
-  );
+	const [value, setValue] = useState("");
+	const [todos, setTodos] = useState([
+		{
+			name: "Buy milk",
+			isChecked: true,
+		},
+	]);
+	return (
+		<div>
+			<div>
+				<h1>ToDo</h1>
+				<input
+					type="text"
+					value={value}
+					onChange={event_ => {
+						setValue(event_.target.value);
+					}} //here the video is blech!
+				/>
+				<button
+					disabled={!value}
+					onClick={() => {
+						setTodos([...todos, { name: value, isChecked: false }]);
+						setValue("");
+					}}
+				>
+					Add
+				</button>
+			</div>
+
+			<ul>
+				{todos.map((todo, index) => {
+					return (
+						<li key={index}>
+							<label>
+								<input
+									type="checkbox"
+									checked={todo.isChecked}
+									onChange={() => {
+										console.log(`Item: ${index + 1}`);
+										const update = [...todos];
+										update[index].isChecked = !update[index].isChecked;
+										setTodos(update);
+									}}
+								/>
+								{todo.name}
+							</label>
+							<button
+								onClick={() => {
+									const update = [...todos];
+									update.splice(index, 1);
+									setTodos(update);
+								}}
+							>
+								Delete
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default App;
