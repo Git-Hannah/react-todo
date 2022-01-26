d6601732db339cbe2db9c4f11345775ce85332b4
HannaHannah <hannahsenft@gmx.net>
2022-01-26 14:46:29 +0100

    move nodemon and express to dev dependencies

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-import express from 'express';
+import express from "express";
 
-const app = express()
-const port = 4000
+const app = express();
+const port = 4000;
 
-app.get('/', (request, response) => {
-	response.send('Hello World!')
-  })
+app.get("/", (request, response) => {
+	response.send("Hello World!");
+});
 
+app.get("/todos/", (request, response) => {
+	response.json([
+		{ id: 1, name: "First toDo" },
+		{ id: 2, name: "Second toDo" },
+	]);
+});
 
-  app.get('/todos/', (request, response) => {
-	  response.json([
-			  { id: 1, name: "First toDo"},
-		  { id: 2, name: "Second toDo"},
-	  ])
-  })
-
-  app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+	console.log(`ToDoApp is listening on port ${port}`);
+});
