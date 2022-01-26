0b48cf572ce4423721c0529d20568f7d7e999ced
HannaHannah <hannahsenft@gmx.net>
2022-01-26 16:37:52 +0100

    add post request variants

--- server.js
+++ server.js
@@ -1,17 +1,64 @@
 import express from "express";
+import { readFile, writeFile } from 'fs/promises';
 
 const app = express();
 const port = 4000;
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
 	response.send("Hello World!");
 });
 
-app.get("/todos/", (request, response) => {
-	response.json([
-		{ id: 1, name: "First toDo" },
-		{ id: 2, name: "Second toDo" },
-	]);
+
+
+const data = await readFile("./database/database.json", "utf8");
+const json = JSON.parse(data);
+console.log(json);
+
+// Variante 1, .then() - chaining :
+app.get("/api/todos/", (request, response) => {
+	readFile("./database/database.json", "utf8")
+	.then(data => JSON.parse(data))
+	.then(json => {
+		response.json(json.todos);
+	})
+});
+
+// Variante 2, slight refactoring, only one .then() :
+app.get("/api/todos", (request, response) => {
+	readFile("./database/database.json", "utf8")
+	.then(data => {
+		const json = JSON.parse(data);
+		response.json(json.todos);
+	});
+});
+
+// Variante 3, async/await wird deutlich :
+app.get("/api/todos", async (request, response) => {
+	const data = await readFile("./database/database.json", "utf8");
+	const json = JSON.parse(data);
+	response.json(json.todos);
+});
+
+
+const DATABASE_URI = "./database/database.json";
+
+app.post("/api/todos/", async (request, response) => {
+	// console.log(request.body)
+	const data = await readFile(DATABASE_URI, "utf8");
+	const json = JSON.parse(data);
+
+	const todo = {
+		...request.body,
+		id: "something-unique",
+	};
+
+	json.todos.push(todo);
+	await writeFile(DATABASE_URI, JSON.stringify(json));
+	response.status(201);
+	response.json(todo);
+
 });
 
 app.listen(port, () => {
