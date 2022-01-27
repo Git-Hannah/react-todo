962a4bd70e57dc5bbe4f10bb25deafe35e4f2c82
HannaHannah <hannahsenft@gmx.net>
2022-01-27 11:48:04 +0100

    feat: Add database connection function

---
+++ utils/database.js
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb';
+
+let client
+
+export async function connectDatabse(url) {
+	client = new MongoClient(url);
+	await client.connect();
+}
+
+export function getTodoCollection() {
+	client.db().collection('todos');
+}
+
+// export function getTodoCollection(name) {
+// 	client.db().collection(name);
+// }
+
