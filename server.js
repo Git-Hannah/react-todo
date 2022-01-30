22c1512d4967db374de69e816b674c89a41e8f68
HannaHannah <hannahsenft@gmx.net>
2022-01-30 18:48:07 +0100

    add feature/mongoose

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (!process.env.MONGODB_URI) {
 }
 
 const app = express();
-const port = 4000;
+const port = 1337;
 
 app.use(express.json());
 app.use(cors);
@@ -27,121 +27,121 @@ app.get("/", (request, response) => {
 	response.send("Hello World!");
 });
 
-// Variante 1, .then() - chaining :
-/* app.get("/api/todos/", (request, response) => {
-	readFile("./database/database.json", "utf8")
-	.then(data => JSON.parse(data))
-	.then(json => {
-		response.json(json.todos);
-	})
-}); */
+// // Variante 1, .then() - chaining :
+// /* app.get("/api/todos/", (request, response) => {
+// 	readFile("./database/database.json", "utf8")
+// 	.then(data => JSON.parse(data))
+// 	.then(json => {
+// 		response.json(json.todos);
+// 	})
+// }); */
 
-// Variante 2, slight refactoring, only one .then() :
-/* app.get("/api/todos", (request, response) => {
-	readFile("./database/database.json", "utf8")
-	.then(data => {
-		const json = JSON.parse(data);
-		response.json(json.todos);
-	});
-}); */
+// // Variante 2, slight refactoring, only one .then() :
+// /* app.get("/api/todos", (request, response) => {
+// 	readFile("./database/database.json", "utf8")
+// 	.then(data => {
+// 		const json = JSON.parse(data);
+// 		response.json(json.todos);
+// 	});
+// }); */
 
-const DATABASE_URI = "./database/database.json";
+// const DATABASE_URI = "./database/database.json";
 
-// Variante 3, async/await wird deutlich :
-app.get("/api/todos", async (request, response, next) => {
-	try {
-		const data = await readFile(DATABASE_URI, "utf8");
-		const json = JSON.parse(data);
-		response.json(json.todos);
-	} catch (error_) {
-		next(error_);
-	}
-});
+// // Variante 3, async/await wird deutlich :
+// app.get("/api/todos", async (request, response, next) => {
+// 	try {
+// 		const data = await readFile(DATABASE_URI, "utf8");
+// 		const json = JSON.parse(data);
+// 		response.json(json.todos);
+// 	} catch (error_) {
+// 		next(error_);
+// 	}
+// });
 
-app.post("/api/todos", async (request, response, next) => {
-	try {
-		const todo = new Todo({
-			...request.body,
-			isChecked: false,
-		});
-		console.log(todo);
-		const mongoDbResponse = await todo.save();
+// app.post("/api/todos", async (request, response, next) => {
+// 	try {
+// 		const todo = new Todo({
+// 			...request.body,
+// 			isChecked: false,
+// 		});
+// 		console.log(todo);
+// 		const mongoDbResponse = await todo.save();
 
-		response.status(201).send(mongoDbResponse);
-	} catch (error_) {
-		next(error_);
-	}
-});
+// 		response.status(201).send(mongoDbResponse);
+// 	} catch (error_) {
+// 		next(error_);
+// 	}
+// });
 
-/* Alte Variante:
-	app.post("/api/todos/", async (request, response, next) => {
-	// console.log(request.body)
-	const data = await readFile(DATABASE_URI, "utf8");
-	const json = JSON.parse(data);
+// /* Alte Variante:
+// 	app.post("/api/todos/", async (request, response, next) => {
+// 	// console.log(request.body)
+// 	const data = await readFile(DATABASE_URI, "utf8");
+// 	const json = JSON.parse(data);
 
-	const todo = {
-		...request.body,
-		id: uuid(),
-	};
+// 	const todo = {
+// 		...request.body,
+// 		id: uuid(),
+// 	};
 
-	json.todos.push(todo);
-	await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
-	response.status(201);
-	response.json(todo);
-}); */
+// 	json.todos.push(todo);
+// 	await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
+// 	response.status(201);
+// 	response.json(todo);
+// }); */
 
-app.delete("/api/todos", async (request, response, next) => {
-	const { id } = request.body;
-	try {
-		const data = await readFile(DATABASE_URI, "utf-8");
-		const json = JSON.parse(data);
-		const index = json.todos.findIndex(user => user.id === id);
-		if (index < 0) {
-			response.status(400);
-			response.json({ error: { message: "This entry does not exist" } });
-			return;
-		}
-		json.todos.splice(index, 1);
-		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
-		// Send a 204 (No Content)
-		response.status(204);
-		response.send();
-		// Or 200
-		// response.status(200);
-		// response.send("entry deleted");
-	} catch (error_) {
-		next(error_);
-	}
-});
+// app.delete("/api/todos", async (request, response, next) => {
+// 	const { id } = request.body;
+// 	try {
+// 		const data = await readFile(DATABASE_URI, "utf-8");
+// 		const json = JSON.parse(data);
+// 		const index = json.todos.findIndex(user => user.id === id);
+// 		if (index < 0) {
+// 			response.status(400);
+// 			response.json({ error: { message: "This entry does not exist" } });
+// 			return;
+// 		}
+// 		json.todos.splice(index, 1);
+// 		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
+// 		// Send a 204 (No Content)
+// 		response.status(204);
+// 		response.send();
+// 		// Or 200
+// 		// response.status(200);
+// 		// response.send("entry deleted");
+// 	} catch (error_) {
+// 		next(error_);
+// 	}
+// });
 
-// Update:
-app.put("/api/todos", async (request, response, next) => {
-	try {
-		const { id, update } = request.body;
-		const data = await readFile(DATABASE_URI, "utf-8");
-		const json = JSON.parse(data);
-		const index = json.todos.findIndex(user => user.id === id);
-		if (index < 0) {
-			response.status(400);
-			response.json({ error: { message: "This entry does not exist" } });
-			return;
-		}
-		json.todos[index] = { ...json.todos[index], ...update, id };
-		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
-		// Send a 200
-		response.status(200);
-		response.send(json.todos[index]);
-		// Or 204 (No Content)
-		// response.status(204);
-		// response.send();
-	} catch (error_) {
-		next(error_);
-	}
-});
+// // Update:
+// app.put("/api/todos", async (request, response, next) => {
+// 	try {
+// 		const { id, update } = request.body;
+// 		const data = await readFile(DATABASE_URI, "utf-8");
+// 		const json = JSON.parse(data);
+// 		const index = json.todos.findIndex(user => user.id === id);
+// 		if (index < 0) {
+// 			response.status(400);
+// 			response.json({ error: { message: "This entry does not exist" } });
+// 			return;
+// 		}
+// 		json.todos[index] = { ...json.todos[index], ...update, id };
+// 		await writeFile(DATABASE_URI, JSON.stringify(json, null, 4));
+// 		// Send a 200
+// 		response.status(200);
+// 		response.send(json.todos[index]);
+// 		// Or 204 (No Content)
+// 		// response.status(204);
+// 		// response.send();
+// 	} catch (error_) {
+// 		next(error_);
+// 	}
+// });
 
-app.put("/api/todos", async (request, response, next) => {
-	//kein data load von DB
-});
+// app.put("/api/todos", async (request, response, next) => {
+// 	//kein data load von DB
+// });
 
 mongoose.connect(process.env.MONGODB_URI).then(() => {
 	app.listen(port, () => {
